afdfbf0487f407c20503e68c1c9c93b37f2af900
Gabriele Petronella <gabriele@buildo.io>
2014-09-16 03:37:36 +0200

    Add proper test for macro expansion

--- src/test/scala/spray/json/AutoProductFormat.scala
+++ src/test/scala/spray/json/AutoProductFormat.scala
@@ -5,12 +5,16 @@ import org.scalatest.{ Matchers, WordSpec }
 case class ID[T](t: String)
 case class Thing(id: ID[Thing])
 
-class AutoProductFormatSpec {
+class AutoProductFormatSpec extends WordSpec with Matchers {
   // Test for compilation
-  implicit object ThingAutoProductFormattable extends AutoProductFormattable[Thing]
-  implicit def labOnlineIdFormat[T <: ID[_]] = new JsonFormat[T] {
-    def write(id: T) = ???
-    def read(idRep: JsValue): T = ???
+  "AutoProductFormatMacro" should {
+    "work with higher-kinded types" in {
+      implicit object ThingAutoProductFormattable extends AutoProductFormattable[Thing]
+      implicit def labOnlineIdFormat[T <: ID[_]] = new JsonFormat[T] {
+        def write(id: T) = ???
+        def read(idRep: JsValue): T = ???
+      }
+      "AutoProductFormat.jsonFormat[Thing]" should compile
+    }
   }
-  AutoProductFormat.jsonFormat[Thing]
 }
