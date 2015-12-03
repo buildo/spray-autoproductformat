c0a932ec6c3fcfdf7226c66a7e5434f8164273fc
Federico Pellegatta <federico.pellegatta@omnilab.it>
2015-12-03 12:32:53 +0100

    Test is now using the explict autoProductSerialize[T] method of AutoProductFormat.

--- src/test/scala/spray/json/AutoProductFormat.scala
+++ src/test/scala/spray/json/AutoProductFormat.scala
@@ -9,12 +9,11 @@ class AutoProductFormatSpec extends WordSpec with Matchers {
   // Test for compilation
   "AutoProductFormatMacro" should {
     "work with higher-kinded types" in {
-      implicit object ThingAutoProductFormattable extends AutoProductFormattable[Thing]
       implicit def labOnlineIdFormat[T <: ID[_]] = new JsonFormat[T] {
         def write(id: T) = ???
         def read(idRep: JsValue): T = ???
       }
-      "AutoProductFormat.jsonFormat[Thing]" should compile
+      "AutoProductFormat.autoProductSerialize[Thing]" should compile
     }
   }
 }
